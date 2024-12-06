02adb879df6f46dfd392b17d067a83a7de9bb909
diasxz1 <joaovdiasdossantos@gmail.com>
2024-12-06 11:07:16 -0300

    deletar_produto.php

--- deletar_produto.php
+++ deletar_produto.php
@@ -4,13 +4,13 @@ include 'conexao.php';
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Preparando e executando a exclusão do produto no banco de dados
+    
     $sql = "DELETE FROM produtos WHERE id = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $id);
 
     if ($stmt->execute()) {
-        header("Location: gerenciar_produtos.php"); // Redireciona de volta para a página de gerenciamento de produtos
+        header("Location: gerenciar_produtos.php"); 
         exit();
     } else {
         echo "Erro ao deletar produto.";
