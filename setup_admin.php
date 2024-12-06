3f1f4e56aa12b466fd7f7516b1600337b5b9181a
diasxz1 <joaovdiasdossantos@gmail.com>
2024-12-06 11:14:30 -0300

    setup_admin.php

--- setup_admin.php
+++ setup_admin.php
@@ -1,14 +1,14 @@
 <?php
-include 'conexao.php'; // Inclui a conexão com o banco de dados
+include 'conexao.php'; 
 
-// Verifica se o administrador já existe no banco
+
 $sql = "SELECT * FROM usuarios WHERE username = 'admin'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "O usuário administrador já existe.";
 } else {
-    // Insere o administrador no banco com uma senha simples
+    
     $sql = "INSERT INTO usuarios (username, password, cargo) VALUES ('admin', 'admin@123123', 'admin')";
 
     if ($conn->query($sql) === TRUE) {
