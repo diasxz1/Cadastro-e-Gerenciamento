d6d1621a64bec3b729394c24c5be3e1a9c9f679b
diasxz1 <joaovdiasdossantos@gmail.com>
2024-12-06 11:10:14 -0300

    conexao.php

--- conexao.php
+++ conexao.php
@@ -4,10 +4,10 @@ $username = "root";
 $password = "";
 $dbname = "sistema_jogos";
 
-// Conexão com o banco de dados
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verifica a conexão
+
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
